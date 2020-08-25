0f98e2c012b1e686d377cceef4cb7c5a4a9f4209
13952522076 <xuma@my.unt.edu>
2020-08-25 02:23:00 -0500

    update

--- runing.sh
+++ runing.sh
@@ -1,4 +1,23 @@
 python3 cifar.py --netName MobileNetV2 --cifar 100 --bs 512
 python3 cifar.py --netName GEMobileNetV2 --cifar 100 --bs 512
+python3 cifar.py --netName SPA2MobileNetV2 --cifar 100 --bs 512
+python3 cifar.py --netName SPA3MobileNetV2 --cifar 100 --bs 512
+python3 cifar.py --netName SPA4MobileNetV2 --cifar 100 --bs 512
+
+python3 cifar.py --netName SEResNet18 --cifar 100 --bs 512
+python3 cifar.py --netName SPAResNet18 --cifar 100 --bs 512
+python3 cifar.py --netName GEResNet18 --cifar 100 --bs 512
+
+python3 cifar.py --netName ResNeXt29 --cifar 100 --bs 512
+python3 cifar.py --netName SEResNeXt29 --cifar 100 --bs 512
+python3 cifar.py --netName GEResNeXt29 --cifar 100 --bs 512
+
+python3 cifar.py --netName VGG16 --cifar 100 --bs 512
+python3 cifar.py --netName SEVGG16 --cifar 100 --bs 512
+python3 cifar.py --netName GEVGG16 --cifar 100 --bs 512
+
+python3 cifar.py --netName dense --cifar 100 --bs 512
+python3 cifar.py --netName se_dense --cifar 100 --bs 512
+python3 cifar.py --netName ge_dense --cifar 100 --bs 512
 
 
