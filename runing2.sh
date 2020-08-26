21ecee97334846dc0360846b9138c995c07d0978
13952522076 <xuma@my.unt.edu>
2020-08-25 19:35:03 -0500

    update

--- runing2.sh
+++ runing2.sh
@@ -1,7 +1,4 @@
-
-
-python3 cifar.py --netName ge_dense --cifar 100 --bs 512
-python3 cifar.py --netName se_dense --cifar 100 --bs 512
-python3 cifar.py --netName dense --cifar 100 --bs 512
+python3 cifar.py --netName spa_b_dense --cifar 100 --bs 512
+python3 cifar.py --netName spa_c_dense --cifar 100 --bs 512
 
 
