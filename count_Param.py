e4a5368e60b2b1528de20312dd71c5ba19156661
13952522076 <xuma@my.unt.edu>
2020-08-26 14:52:28 -0500

    update

--- count_Param.py
+++ count_Param.py
@@ -1,8 +1,8 @@
 from utils import get_model_complexity_info
-import models_cifar as models
+import models_imagenet as models
 import torchvision.models
 
-model = models.__dict__['prm_mnasnet1_0'](num_classes=1000)
+model = models.__dict__['spa_resnet50'](num_classes=1000)
 
 flops, params = get_model_complexity_info(model, (224, 224), as_strings=False, print_per_layer_stat=False)
 print('Flops:  %.3f' % (flops / 1e9))
--- models_imagenet/__init__.py
+++ models_imagenet/__init__.py
@@ -4,3 +4,4 @@ from __future__ import absolute_import
 from .resnet_old import *
 
 from .resnet_spa import *
+from .resnet_spa2 import *
