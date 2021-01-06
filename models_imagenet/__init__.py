3fe6dd0356a0f5388b729e090f8dd0e997e36514
13952522076 <xuma@my.unt.edu>
2021-01-05 21:39:55 -0500

    update ablation (revised)

--- models_imagenet/__init__.py
+++ models_imagenet/__init__.py
@@ -6,3 +6,9 @@ from .resnet_old import *
 from .resnet_spa import *
 from .resnet_spa2 import *
 from .mobilenet_spa import *
+
+
+# ablation study (revised)
+from .resnet_spa_12 import spa_12_resnet50
+from .resnet_spa_124 import spa_124_resnet50
+from .resnet_spa_1247 import spa_1247_resnet50
